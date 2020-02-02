bc42eaf05cace11a14b243fb3a28b4208794d376
Francisco Gonzalez <pacog879@hotmail.com>
2020-02-02 17:41:21 -0600

    fixed updating text & placement on world

--- Team Game 1/Assets/Scripts/PlayerHealth.cs
+++ Team Game 1/Assets/Scripts/PlayerHealth.cs
@@ -8,11 +8,14 @@ public class PlayerHealth : MonoBehaviour
     float lives = 4;
     string screenUi;
     TMP_Text screenText;
+    GameObject playerHealth;
     // Start is called before the first frame update
     void Start()
     {
         screenText = GameObject.Find("PlayerHealth").GetComponent<TMP_Text>();
+        playerHealth = GameObject.Find("PlayerHealth");
         screenText.text = "Health: " + lives;
+        playerHealth.transform.position = Camera.main.ViewportToWorldPoint(new Vector3(0.6f, 0.7f, 10f));
         Debug.Log("SCREENUI: " + screenText.text);
     }
 
