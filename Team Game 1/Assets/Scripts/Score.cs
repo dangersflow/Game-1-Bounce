b52f9031c9dc944ed8cfc20f52eb45a2a990b82c
Alvaro Rios <alvarorios12@gmail.com>
2020-02-03 10:32:39 -0600

    Score keeping added! Just based off time.DeltaTime * 100

---
+++ Team Game 1/Assets/Scripts/Score.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Score : MonoBehaviour
+{   
+    int score;
+    TMP_Text text;
+    GameObject scoreObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GameObject.Find("Score").GetComponent<TMP_Text>();
+        scoreObject = GameObject.Find("Score");
+        score = 0;   
+        text.text = "Score: " + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score += (int)(Time.deltaTime * 100);
+        text.text = "Score: " + score;
+        //Debug.Log("Score: " + score);
+    }
+}
+
